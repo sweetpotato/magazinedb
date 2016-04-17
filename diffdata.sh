468e8bbda65658555811c574487f2315b0c2ede2
sweetpotato <sweetpotato@users.noreply.github.com>
2016-04-17 20:24:29 +0900

    diffdata: Diff old numbers

--- diffdata.sh
+++ diffdata.sh
@@ -4,7 +4,9 @@ set -xue
 set -o pipefail
 
 [ -n "$1" ]
+prefix="$1"
+index="${2:-0}"
 
-find data -name "$1-*" | sort -r | head -n 2 |\
+find data -name "$prefix-*" | sort -r | head -n "$((index+2))" | tail -n 2 |\
   xargs -n 2 bash -c 'diff -u <(sort "$1") <(sort "$0")' |\
   grep -E '^[-+]' | grep -vE '^[-+]{3}'
