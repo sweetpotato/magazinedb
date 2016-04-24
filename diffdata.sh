a6c51480ebc2945fd409023a68fa34ac288856ca
sweetpotato <sweetpotato@users.noreply.github.com>
2016-04-24 13:48:24 +0900

    diffdata: Add some find options not to search into subdirectories

--- diffdata.sh
+++ diffdata.sh
@@ -7,6 +7,6 @@ set -o pipefail
 prefix="$1"
 index="${2:-0}"
 
-find data -name "$prefix-*" | sort -r | head -n "$((index+2))" | tail -n 2 |\
+find data -mindepth 1 -maxdepth 1 -name "$prefix-*" | sort -r | head -n "$((index+2))" | tail -n 2 |\
   xargs -n 2 bash -c 'diff -u <(sort "$1") <(sort "$0")' |\
   grep -E '^[-+]' | grep -vE '^[-+]{3}'
