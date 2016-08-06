59248a72566e16326798e632e928b37ebdd8d19a
sweetpotato <sweetpotato@users.noreply.github.com>
2016-08-06 23:38:23 +0900

    Add 4 master files and MyVars files

---
+++ MyVars/04-CLU.pm
@@ -0,0 +1,10 @@
+package MyVars;
+use base Exporter;
+our @EXPORT = qw(LoadPattern LoadKeys);
+
+# U+FF0F FULLWIDTH SOLIDUS (FULLWIDTH SLASH)
+# U+FF1C FULLWIDTH LESS-THAN SIGN
+sub LoadPattern { qr!(.*?)\x{FF0F}(.*?)(:?\x{FF1C}|$)! }
+sub LoadKeys { [qw(title author)] }
+
+1;
