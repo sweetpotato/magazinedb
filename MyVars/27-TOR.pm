877aadff9cf238ae6fe333672f66aa2ef722b50a
sweetpotato <sweetpotato@users.noreply.github.com>
2016-09-28 20:21:01 +0900

    Fix rc and MyVars of Manga Time Original

--- MyVars/27-TOR.pm
+++ MyVars/27-TOR.pm
@@ -2,7 +2,8 @@ package MyVars;
 use base Exporter;
 our @EXPORT = qw(LoadPattern LoadKeys);
 
-sub LoadPattern { qr!^(.*?)$! }
-sub LoadKeys { [qw(title)] }
+# U+3000 IDEOGRAPHIC SPACE (FULLWIDTH SPACE)
+sub LoadPattern { qr!^(.*)\x{3000}(.*?)$! }
+sub LoadKeys { [qw(title author)] }
 
 1;
